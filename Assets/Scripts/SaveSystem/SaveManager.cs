5af35a4ccfe34c12b5660d41c394842d20eac668
Nicl1909 <108192042+Nicl1909@users.noreply.github.com>
2026-07-21 22:30:33 +0200

    Update SaveManager.cs

--- Assets/Scripts/SaveSystem/SaveManager.cs
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public SaveData saveData;
+    // Awake is called when the script instance is being loaded
+    void Awake()
     {
-        
+        loadCharacterData();    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void loadCharacterData()
     {
-        
+        string characterName = PlayerPrefs.GetString("CharacterName", "DefaultName");
+        int characterLevel = PlayerPrefs.GetInt("CharacterLevel", 1);
+        float characterHealth = PlayerPrefs.GetFloat("CharacterHealth", 100f);
+        float characterMana = PlayerPrefs.GetFloat("CharacterMana", 50f);
+        int characterExperience = PlayerPrefs.GetInt("CharacterExperience", 0);
+
+        CharacterData characterData = new CharacterData
+        {
+            characterName = characterName,
+            characterLevel = characterLevel,
+            characterHealth = characterHealth,
+            characterMana = characterMana,
+            characterExperience = characterExperience
+        };
+
+        Debug.Log($"Loaded Character Data: Name={characterData.characterName}, Level={characterData.characterLevel}, Health={characterData.characterHealth}, Mana={characterData.characterMana}, Experience={characterData.characterExperience}");
     }
+
 }
