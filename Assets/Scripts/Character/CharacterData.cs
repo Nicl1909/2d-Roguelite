5c235d8933b85e03feb9ea855cbf87c2738ab81f
CookieMonster <95055143+CookieMonster760@users.noreply.github.com>
2026-07-21 23:39:15 +0200

    All the files

--- Assets/Scripts/Character/CharacterData.cs
+++ Assets/Scripts/Character/CharacterData.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class CharacterData
 {
-    
+    public string characterName { get; set; }
+    public int characterLevel { get; set; }
+    public float characterHealth { get; set; }
+    public float characterMana { get; set; }
+    public int characterExperience { get; set; }
 }
