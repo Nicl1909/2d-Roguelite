61951f83315ea4941924052fa0f46d79463e1306
Mathe0101 <Helipilot08@web.de>
2026-07-21 03:38:38 +0200

    Player Dash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
-    public float horizontal;
-    public float vertical;
+    private float horizontal;
+    private float vertical;
     public float speed;
+
+    public bool canDash = true;
+    public bool isDashing;
+    public float dashingPower;
+    public float dashingTime;
+    public float dashingCooldown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,8 +23,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
         horizontal = Input.GetAxisRaw("Horizontal") * speed;
         vertical = Input.GetAxisRaw("Vertical") * speed;
-        rigidbody.linearVelocity = new Vector2(horizontal,vertical);
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    //Runs 50 times per second
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        rigidbody.linearVelocity = new Vector2(horizontal, vertical);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rigidbody.linearVelocity = new Vector2(dashingPower * (horizontal / (Mathf.Abs(horizontal) + Mathf.Abs(vertical))), dashingPower * (vertical / (Mathf.Abs(horizontal) + Mathf.Abs(vertical))));
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
